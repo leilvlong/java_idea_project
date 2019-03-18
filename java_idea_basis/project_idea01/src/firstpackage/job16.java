e648ff5d430cbceacdf34498c044da9cac56538a
leilvlong <1455013965@qq.com>
2019-03-18 16:40:35 +0800

    lambda 表达式

--- java_idea_basis/project_idea01/src/firstpackage/job16.java
+++ java_idea_basis/project_idea01/src/firstpackage/job16.java
@@ -1,14 +1,37 @@
 package firstpackage;
+
+import java.util.ArrayList;
+
 /*
 lambda 表达式示例
 虽然不那么面向,且使用起来诸多限制:
     如接口只能有一个抽象方法
 但是比较优雅:
     ()->就可以代替整个匿名内部类
+
+使用lambda的场景:
+    抽象类或者接口只有一个方法,而方法的参数中 为该抽象类(接口)的实现子类参数 即可使用
+    例1:
+        arrays.removeif() 的参数为:  Predicate<? super E> filter:
+            - <? extends E>:固定泛型的上边界, 泛型可以是E, 也可以是E的子类
+            - <? super E>: 固定泛型的下边界, 泛型可以是E, 也可以是E的父类
+        在该方法中 传入一个lambda表达式 java会将其视为该接口的实现方法
+    例2:
+        开启线程时:
+        new Thread(()->{
+           for (int x = 0; x < 100; x++) {
+               System.out.println("AAAAAAAAAAAAAAAAAAAA");
+           }
+       }).start();
+        new Thread(()->{
+            for (int x = 0; x < 100; x++) {
+                System.out.println("111111111111111111111");
+            }
+        }).start();
  */
 public class job16 {
     public static void main(String[] args) {
-        int[] ints = {1,2,3,4};
+        /*int[] ints = {1,2,3,4};
         System.out.println("lambda表达式________________________");
         int sum1 = fun1(ints,ints1 ->{
             int sum=0;
@@ -36,7 +59,28 @@ public class job16 {
                 return sum;
             }
         });
-        System.out.println(sum3);
+        System.out.println(sum3);*/
+
+       /* ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.removeIf((num)->{
+            return num>3;
+        });
+        System.out.println(arr);*/
+
+       new Thread(()->{
+           for (int x = 0; x < 100; x++) {
+               System.out.println("AAAAAAAAAAAAAAAAAAAA");
+           }
+       }).start();
+        new Thread(()->{
+            for (int x = 0; x < 100; x++) {
+                System.out.println("111111111111111111111");
+            }
+        }).start();
 
     }
 
